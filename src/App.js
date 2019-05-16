605c3ae85c6d4796346a36fd9d1e9edfbe493a5a
MCVNick <mcvnick@gmail.com>
2019-05-16 12:55:58 -0600

    changes header and fades other selection on hover

--- src/App.js
+++ src/App.js
@@ -7,78 +7,112 @@ const boyScoutsLogo = require('./resources/boyScoutsLogo.png')
 const cubScoutsBackground = require('./resources/test1.jpg')
 const boyScoutsBackground = require('./resources/test2.jpg')
 
-function handleHover(cub, divide, boy, free, choice) {
-  const cub2 = cub.current.classList
-  const divide2 = divide.current.classList
-  const boy2 = boy.current.classList
-  
-  console.log(free, cub, divide, boy, choice.current)
-  if (choice.current !== null) {
+function handleHover(dataObj) {
+  let { cub, cubLogo, divide, boy, boyLogo, choice, defaultHeader, packHeader, troopHeader } = dataObj
+  const cubList = cub.current.classList
+  const divideList = divide.current.classList
+  const boyList = boy.current.classList
+  const cubLogoList = cubLogo.current.classList
+  const boyLogoList = boyLogo.current.classList
+  const defaultHeaderList = defaultHeader.current.classList
+  const packHeaderList = packHeader.current.classList
+  const troopHeaderList = troopHeader.current.classList
+
+  if (choice !== null) {
     setTimeout(() => {
-      handleHover(cub, divide, boy, free, choice)
+      handleHover(dataObj)
     }, 600);
   }
 
-  if (!free.current) {
-    if(choice.current === 'cub') {
-      cub2.add('background-fill')
-      cub2.add('delay-background-leave')
-      divide2.add('divide-slide-right')
-      boy2.add('background-leave')
+  if (!dataObj.free) {
+    if (choice === 'cub') {
+      cubList.add('background-fill')
+      cubList.add('delay-background-leave')
+      divideList.add('divide-slide-right')
+      boyList.add('background-leave')
+      boyLogoList.add('fade-out')
+      packHeaderList.remove('fade-out')
+      defaultHeaderList.add('fade-out')
+      troopHeaderList.add('fade-out')
     }
-    else if (choice.current === 'boy'){
-      boy2.add('background-fill')
-      boy2.add('delay-background-leave')
-      divide2.add('divide-slide-left')
-      cub2.add('background-leave')
+    else if (choice === 'boy') {
+      boyList.add('background-fill')
+      boyList.add('delay-background-leave')
+      divideList.add('divide-slide-left')
+      cubList.add('background-leave')
+      cubLogoList.add('fade-out')
+      troopHeaderList.remove('fade-out')
+      defaultHeaderList.add('fade-out')
+      packHeaderList.add('fade-out')
     }
     else {
-      free.current = 1
-      divide2.remove('divide-slide-left')
-      divide2.remove('divide-slide-right')
-      cub2.remove('background-fill')
-      boy2.remove('background-fill')
+      dataObj.free = 1
+      divideList.remove('divide-slide-left')
+      divideList.remove('divide-slide-right')
+      cubList.remove('background-fill')
+      boyList.remove('background-fill')
       setTimeout(() => {
-        cub2.remove('delay-background-leave')
-        boy2.remove('delay-background-leave')
-        boy2.remove('background-leave')
-        cub2.remove('background-leave')
-        free.current = null
+        cubList.remove('delay-background-leave')
+        boyList.remove('delay-background-leave')
+        boyList.remove('background-leave')
+        cubList.remove('background-leave')
+        boyLogoList.remove('fade-out')
+        cubLogoList.remove('fade-out')
+        defaultHeaderList.remove('fade-out')
+        packHeaderList.add('fade-out')
+        troopHeaderList.add('fade-out')
+        dataObj.free = null
       }, 500);
     }
   }
 }
 
 function App() {
-  let cub = createRef()
-  let divide = createRef()
-  let boy = createRef()
-  let free = createRef()
-  let choice = createRef()
+  const dataObj = {
+    cub: createRef(),
+    cubLogo: createRef(),
+    divide: createRef(),
+    boy: createRef(),
+    boyLogo: createRef(),
+    defaultHeader: createRef(),
+    packHeader: createRef(),
+    troopHeader: createRef(),
+    free: null,
+    choice: null
+  }
+  let { cub, cubLogo, divide, boy, boyLogo, defaultHeader, packHeader, troopHeader } = dataObj
 
   return (
     <div className="App">
-      <h1 className='scout-header'>799 Scouts</h1>
+      <h1 ref={defaultHeader} className='scout-header'>799 Scouts</h1>
+      <h1 ref={packHeader} className='scout-header fade-out'>Pack 799</h1>
+      <h1 ref={troopHeader} className='scout-header fade-out'>Troop 799 </h1>
       <div className='container'>
         <p className='req-220px'>You need at least 220px to view this website</p>
-        <div onMouseEnter={() => {choice.current = 'cub'; handleHover(cub, divide, boy, free, choice)}} onMouseLeave={() => {choice.current = null; handleHover(cub, divide, boy, free, choice)}}>
-          <img className='cub-scouts-logo' src={cubScoutsLogo} alt='cubScoutsLogo'/>
+        <div
+          onMouseEnter={() => { dataObj.choice = 'cub'; handleHover(dataObj) }}
+          onMouseLeave={() => { dataObj.choice = null; handleHover(dataObj) }}
+        >
+          <img ref={cubLogo} className='cub-scouts-logo' src={cubScoutsLogo} alt='cubScoutsLogo' />
         </div>
-        <div onMouseEnter={() => {choice.current = 'boy'; handleHover(cub, divide, boy, free, choice)}} onMouseLeave={() => {choice.current = null; handleHover(cub, divide, boy, free, choice)}}>
-          <img className='boy-scouts-logo' src={boyScoutsLogo} alt='boyScoutsLogo'/>
+        <div
+          onMouseEnter={() => { dataObj.choice = 'boy'; handleHover(dataObj) }}
+          onMouseLeave={() => { dataObj.choice = null; handleHover(dataObj) }}
+        >
+          <img ref={boyLogo} className='boy-scouts-logo' src={boyScoutsLogo} alt='boyScoutsLogo' />
         </div>
       </div>
 
       <div className='background'>
         <div ref={cub} className='top'>
           <div>
-            <img src={cubScoutsBackground} alt='cubScoutsBackground'/>
+            <img src={cubScoutsBackground} alt='cubScoutsBackground' />
           </div>
         </div>
         <div ref={divide} className='divide'></div>
         <div ref={boy} className='bottom'>
           <div>
-            <img src={boyScoutsBackground} alt='boyScoutsBackground'/>        
+            <img src={boyScoutsBackground} alt='boyScoutsBackground' />
           </div>
         </div>
       </div>
