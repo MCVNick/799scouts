4bbeb022bedcc04c7219f24a22a33243f97b093a
MCVNick <mcvnick@gmail.com>
2019-05-15 21:55:10 -0600

    made it so that you can not fling the black line in crazy directions

--- src/App.js
+++ src/App.js
@@ -7,30 +7,45 @@ const boyScoutsLogo = require('./resources/boyScoutsLogo.png')
 const cubScoutsBackground = require('./resources/test1.jpg')
 const boyScoutsBackground = require('./resources/test2.jpg')
 
-function handleHover(cub, divide, boy, choice) {
-  if(choice === 'cub') {
-    cub.current.classList.add('background-fill')
-    cub.current.classList.add('delay-background-leave')
-    divide.current.classList.add('divide-slide-right')
-    boy.current.classList.add('background-leave')
-  }
-  else if (choice === 'boy'){
-    boy.current.classList.add('background-fill')
-    boy.current.classList.add('delay-background-leave')
-    divide.current.classList.add('divide-slide-left')
-    cub.current.classList.add('background-leave')
-  }
-  else {
-    cub.current.classList.remove('background-fill')
-    boy.current.classList.remove('background-fill')
+function handleHover(cub, divide, boy, free, choice) {
+  const cub2 = cub.current.classList
+  const divide2 = divide.current.classList
+  const boy2 = boy.current.classList
+  
+  console.log(free, cub, divide, boy, choice.current)
+  if (choice.current !== null) {
     setTimeout(() => {
-      cub.current.classList.remove('delay-background-leave')
-      boy.current.classList.remove('delay-background-leave')
-      boy.current.classList.remove('background-leave')
-      cub.current.classList.remove('background-leave')
-    }, 500);
-    divide.current.classList.remove('divide-slide-left')
-    divide.current.classList.remove('divide-slide-right')
+      handleHover(cub, divide, boy, free, choice)
+    }, 600);
+  }
+
+  if (!free.current) {
+    if(choice.current === 'cub') {
+      cub2.add('background-fill')
+      cub2.add('delay-background-leave')
+      divide2.add('divide-slide-right')
+      boy2.add('background-leave')
+    }
+    else if (choice.current === 'boy'){
+      boy2.add('background-fill')
+      boy2.add('delay-background-leave')
+      divide2.add('divide-slide-left')
+      cub2.add('background-leave')
+    }
+    else {
+      free.current = 1
+      divide2.remove('divide-slide-left')
+      divide2.remove('divide-slide-right')
+      cub2.remove('background-fill')
+      boy2.remove('background-fill')
+      setTimeout(() => {
+        cub2.remove('delay-background-leave')
+        boy2.remove('delay-background-leave')
+        boy2.remove('background-leave')
+        cub2.remove('background-leave')
+        free.current = null
+      }, 500);
+    }
   }
 }
 
@@ -38,16 +53,18 @@ function App() {
   let cub = createRef()
   let divide = createRef()
   let boy = createRef()
+  let free = createRef()
+  let choice = createRef()
 
   return (
     <div className="App">
-      <h1 className='scout-header'>799 Scouting</h1>
+      <h1 className='scout-header'>799 Scouts</h1>
       <div className='container'>
         <p className='req-220px'>You need at least 220px to view this website</p>
-        <div onMouseEnter={() => {handleHover(cub, divide, boy, 'cub')}} onMouseLeave={() => {handleHover(cub, divide, boy)}}>
+        <div onMouseEnter={() => {choice.current = 'cub'; handleHover(cub, divide, boy, free, choice)}} onMouseLeave={() => {choice.current = null; handleHover(cub, divide, boy, free, choice)}}>
           <img className='cub-scouts-logo' src={cubScoutsLogo} alt='cubScoutsLogo'/>
         </div>
-        <div onMouseEnter={() => {handleHover(cub, divide, boy, 'boy')}} onMouseLeave={() => {handleHover(cub, divide, boy)}}>
+        <div onMouseEnter={() => {choice.current = 'boy'; handleHover(cub, divide, boy, free, choice)}} onMouseLeave={() => {choice.current = null; handleHover(cub, divide, boy, free, choice)}}>
           <img className='boy-scouts-logo' src={boyScoutsLogo} alt='boyScoutsLogo'/>
         </div>
       </div>
